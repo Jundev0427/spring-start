0c16cd7d87936a4cfa50d643d88175ac9a68a619
jun <jundev0427@gmail.com>
2025-03-14 09:41:09 +0900

    회원가입 로직 및 테스트 작성

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository repository;
+
+    public MemberService(MemberRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * 회원가입
+     */
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        repository.findByName(member.getName())
+                .ifPresent(m1 -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    /**
+     * 전체 회원 조희
+     */
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return repository.findById(memberId);
+    }
+}
